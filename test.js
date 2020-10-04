43c47f74a68b13fea6935e858021260c1c8e9584
Almir Osmanovic <almirosmanovice.eposta@gmail.com>
2020-10-04 23:39:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,29 +1,5 @@
-require('dotenv').config()
-var vault = require("node-vault")(vaultOptions);
-var ns = 'secret/nodejs-vault-demo';
-var vaultOptions = {
-  apiVersion: 'v1', // default
-  endpoint: '${vault_url}',
-  token: '${vault_token}'
-};
 
-// Init vault server, but only if not already initialized
-vault.initialized()
-.then((result) => {
-  console.log(result);
-  return vault.init({ secret_shares: 1, secret_threshold: 1 });
-})
-.then((result) => {
-  console.log(result);
-  vault.token = result.root_token;
-  const key = result.keys[0];
-  return vault.unseal({ secret_shares: 1, key });
-})
-.then((result) => {
-  console.log(result);
-  return vault.write(ns, { message: 'Hello World' });
-})
-.catch((err) => console.error(err.message));
+require('dotenv').config()
 const http = require('http');
 
 // Read the host address and the port from the environment
@@ -34,7 +10,7 @@ const msg = process.env.MSG;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(`{"message": ${msg}} ${vault_url}} `);
+    res.end(`{"message": ${msg}}`);
 });
 
 // Start a TCP server listening for connections on the given port and host
